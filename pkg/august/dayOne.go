fc643fa7c13e8c57afe4c40f4e05b830e72a307d
Brad <Bradkharris@gmail.com>
2023-08-13 15:35:28 -0700

    template

--- pkg/august/dayOne.go
+++ pkg/august/dayOne.go
@@ -7,11 +7,27 @@ import (
 	"net/http"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmplt := template.Must(template.ParseFiles("index.html"))
-		tmplt.Execute(w, nil)
+
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "The Shawshank Redemption", Director: "Frank Darabont"},
+				{Title: "Schindler's List", Director: "Steven Spielberg"},
+				{Title: "Raging Bull", Director: "Martin Scorsese"},
+				{Title: "Casablanca", Director: "Michael Curtiz"},
+			},
+		}
+
+		tmplt.Execute(w, films)
 	}
 	http.HandleFunc("/", h1)
 	log.Fatal(http.ListenAndServe(":8080", nil))
